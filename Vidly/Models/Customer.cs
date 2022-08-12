696a24461fe1afa291979321942d9b3034767408
bkisly <b.kisly@gmail.com>
2022-08-12 18:18:07 +0200

    Added birth date to customer's model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,8 +5,8 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required, StringLength(255)]
-        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(255)] public string Name { get; set; } = string.Empty;
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; } = null!;
         public byte MembershipTypeId { get; set; }
