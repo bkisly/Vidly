f45144ce2e8dfe62d33f4d79a8f5ed1cf4316a1b
bkisly <b.kisly@gmail.com>
2022-08-12 16:44:00 +0200

    Applied annotations to customer model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,8 +1,11 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required, StringLength(255)]
         public string Name { get; set; } = string.Empty;
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; } = null!;
