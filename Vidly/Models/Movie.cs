c753f989e346087a2f07c422f2342c249f30ac29
bkisly <b.kisly@gmail.com>
2022-08-15 01:49:59 +0200

    Added data annotations to the movie model

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,14 +1,16 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Release date")] public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
-        public short NumberInStock { get; set; }
+        [Display(Name = "Number in stock")] public short NumberInStock { get; set; }
 
         public Genre Genre { get; set; } = null!;
-        public byte GenreId { get; set; }
+        [Display(Name = "Genre")] public byte GenreId { get; set; }
     }
 }
