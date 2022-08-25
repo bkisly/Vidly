72697f9c986ec1292be5cf87c42576fb68df6fef
bkisly <b.kisly@gmail.com>
2022-08-26 00:54:34 +0200

    Added client-side view generation

---
+++ Vidly/Models/DataTransferObjects/MembershipTypeDto.cs
@@ -0,0 +1,26 @@
+﻿namespace Vidly.Models.DataTransferObjects
+{
+    public class MembershipTypeDto : IDataTransferObject<MembershipType>
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public MembershipType ConvertToModel()
+        {
+            return new()
+            {
+                Id = Id,
+                Name = Name,
+            };
+        }
+
+        public static MembershipTypeDto ConvertToDto(MembershipType membershipType)
+        {
+            return new()
+            {
+                Id = membershipType.Id,
+                Name = membershipType.Name,
+            };
+        }
+    }
+}
