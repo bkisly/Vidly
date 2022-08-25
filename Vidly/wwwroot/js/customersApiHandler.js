227bfe6ed9d7ef5d0487553eae827b61a8bed7b1
bkisly <b.kisly@gmail.com>
2022-08-26 01:37:21 +0200

    Moved JavaScript code to separate files

---
+++ Vidly/wwwroot/js/customersApiHandler.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    // Feed the table using API GET method
+
+    let dataTable = $("#customers").DataTable({
+        ajax: {
+            url: "/api/customers",
+            dataSrc: ""
+        },
+        columns: [
+            {
+                data: "name",
+                render: function (data, type, customer) {
+                    return "<a href='/customers/edit/" + customer.id + "'>" + customer.name + "</a>";
+                }
+            },
+            {
+                data: "membershipType.name"
+            },
+            {
+                data: "id",
+                render: function (data) {
+                    return "<button class='btn-link js-delete' data-customer-id=" + data + ">Delete</button>";
+                }
+            }
+        ]
+    });
+
+    // When the user clicks the button, delete the customer using API DELETE method
+
+    $("#customers").on("click", ".js-delete", function () {
+        let deleteButton = $(this);
+        if (confirm("Are you sure you want to delete this customer?")) {
+            $.ajax({
+                url: "api/customers/" + deleteButton.attr("data-customer-id"),
+                method: "DELETE",
+                success: function () {
+                    dataTable.row(deleteButton.parents("tr")).remove().draw();
+                }
+            });
+        }
+    });
+});
---
+++ Vidly/wwwroot/js/moviesApiHandler.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    // Feed the table using API GET method
+
+    let dataTable = $("#movies").DataTable({
+        ajax: {
+            url: "/api/movies",
+            dataSrc: ""
+        },
+        columns: [
+            {
+                data: "name",
+                render: function (data, type, movie) {
+                    return "<a href='/movies/edit/" + movie.id + "'>" + movie.title + "</a>";
+                }
+            },
+            {
+                data: "genre.name"
+            },
+            {
+                data: "id",
+                render: function (data) {
+                    return "<button class='btn-link js-delete' data-movie-id='" + data + "'>Delete</button>";
+                }
+            }
+        ]
+    });
+
+    // When the user clicks the button, delete the movie using API DELETE method
+
+    $("#movies").on("click", ".js-delete", function () {
+        let deleteButton = $(this);
+        if (confirm("Are you sure you want to delete this movie?")) {
+            $.ajax({
+                url: "/api/movies/" + deleteButton.attr("data-movie-id"),
+                method: "DELETE",
+                success: function () {
+                    dataTable.row(deleteButton.parents("tr")).remove().draw();
+                }
+            });
+        }
+    })
+})
