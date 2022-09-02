08f21b4864115029e665e7ff9bcf3d8cdc08dc0e
bkisly <b.kisly@gmail.com>
2022-09-02 11:19:37 +0200

    Scaffolded an API for rentals

---
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models.DataTransferObjects;
+
+namespace Vidly.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostRental(RentalDto rentalDto)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ Vidly/Models/DataTransferObjects/RentalDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Vidly.Models.DataTransferObjects
+{
+    public class RentalDto
+    {
+        public int CustomerId { get; set; }
+        public IEnumerable<int> MovieIds { get; set; } = null!;
+    }
+}
